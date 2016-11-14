60cafbbf3b8289f2d060569155b725d4d4b339c0
Scott Burton <scotthburton@gmail.com>
2016-11-14 12:33:27 -0700

    Added sorting info

---
+++ week10/sorting.py
@@ -0,0 +1,50 @@
+"""
+File: sorting.py
+Original Author: Br. Burton, designed to be completed by others.
+
+Sorts a list of numbers.
+"""
+
+def sort(numbers):
+    """
+    Fill in this method to sort the list of numbers
+    """
+    pass
+
+def prompt_for_numbers():
+    """
+    Prompts the user for a list of numbers and returns it.
+    :return: The list of numbers.
+    """
+
+    numbers = []
+    print("Enter a series of numbers, with -1 to quit")
+
+    num = 0
+
+    while num != -1:
+        num = int(input())
+
+        if num != -1:
+            numbers.append(num)
+
+    return numbers
+
+def display(numbers):
+    """
+    Displays the numbers in the list
+    """
+    print("The list is:")
+    for num in numbers:
+        print(num)
+
+def main():
+    """
+    Tests the sorting process
+    """
+    numbers = prompt_for_numbers()
+    sort(numbers)
+    display(numbers)
+
+if __name__ == "__main__":
+    main()
